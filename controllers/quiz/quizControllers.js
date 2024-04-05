cefa50dd3559c35f415ab9d63e5fcfb3d7087e6d
jiml268 <114753310+jiml268@users.noreply.github.com>
2024-04-05 14:25:53 -0400

    change the data being passed to front end

--- controllers/quiz/quizControllers.js
+++ controllers/quiz/quizControllers.js
@@ -4,11 +4,11 @@ const quizController = {
 
     async getCategories(req, res) {
         try {
-        const response = await axios.get('https://opentdb.com/api_category.php')
+          const response = await axios.get('https://opentdb.com/api_category.php')
 res.status(200).json({
         code: 200,
         Massage: `catogories`,
-        data: response.data,
+        data: response.data.trivia_categories,
       });
     } catch (err) {
       res.status(400).json({
@@ -23,7 +23,6 @@ res.status(200).json({
        try {
           
          const { cat_id } = req.body;
-         console.log('cat_id', cat_id)
         const response = await axios.post(`https://opentdb.com/api_count.php?category=${cat_id}`)
    
 res.status(200).json({
@@ -45,14 +44,12 @@ res.status(200).json({
              const { amount, cat_id, diff } = req.body;
              const difficulty = diff?diff:""
         const response = await axios.post(`https://opentdb.com/api.php?amount=${amount}&category=${cat_id}&difficulty=${difficulty}`)
-    console.log("response",response.data);
 res.status(200).json({
         code: 200,
         Massage: `Question`,
-        data: response.data,
+        data: response.data.results,
       });
          } catch (err) {
-           console.log("err",err);
       res.status(400).json({
         code: 400,
         Massage: `No Categries were found`,
